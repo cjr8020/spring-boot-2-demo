ec8540862bdb7658b47bcb143647bed99da6bda9
Chris Raiskin <chris.raiskin@standard.com>
2018-07-11 17:22:01 -0700

    add hello-world resource

---
+++ src/main/java/com/example/demo/rest/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.demo.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @RequestMapping(
+      value = "/hello",
+      method = RequestMethod.GET
+  )
+  public String sayHello() {
+    return "Hello";
+  }
+
+}
