afcfe618a6a739afc7ab991ab386c7cedc397208
cjr <cjr8020@gmail.com>
2018-07-21 17:00:15 -0700

    switch inline sql to string template

--- src/main/java/com/example/demo/da/ActorRepository.java
+++ src/main/java/com/example/demo/da/ActorRepository.java
@@ -1,15 +1,9 @@
 package com.example.demo.da;
 
+import com.example.demo.da.dao.ActorDao;
 import com.example.demo.da.entity.Actor;
-import com.example.demo.da.mappers.ActorMapper;
 import java.util.List;
 import org.jdbi.v3.core.Jdbi;
-import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
-import org.jdbi.v3.sqlobject.customizer.Bind;
-import org.jdbi.v3.sqlobject.customizer.BindBean;
-import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
-import org.jdbi.v3.sqlobject.statement.SqlQuery;
-import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -36,19 +30,23 @@ public class ActorRepository {
   }
 
 
-  @RegisterRowMapper(ActorMapper.class)
-  public  interface ActorDao {
-
-    @SqlUpdate("INSERT INTO DEMO.ACTOR (USERNAME, RESOURCES_REQUESTED, RECORD_VERSION, CREATED_BY, CREATED_TIMESTAMP) values (:username, :resourcesRequested, :recordVersion, :createdBy, :createdTimestamp)")
-    @GetGeneratedKeys
-    long create(@BindBean Actor actor);
-
-    @SqlQuery("SELECT * FROM DEMO.ACTOR WHERE ACTOR_ID = :actorId")
-    Actor read(@Bind("actorId") Long actorId);
-
-    @SqlQuery("SELECT * from DEMO.ACTOR")
-    List<Actor> readAll();
-
-  }
+//  @RegisterRowMapper(ActorMapper.class)
+//  @UseStringTemplateSqlLocator
+//  public  interface ActorDao {
+//
+//    @SqlUpdate
+////    @SqlUpdate("INSERT INTO DEMO.ACTOR (USERNAME, RESOURCES_REQUESTED, RECORD_VERSION, CREATED_BY, CREATED_TIMESTAMP) values (:username, :resourcesRequested, :recordVersion, :createdBy, :createdTimestamp)")
+//    @GetGeneratedKeys
+//    long create(@BindBean Actor actor);
+//
+//    @SqlQuery
+////    @SqlQuery("SELECT * FROM DEMO.ACTOR WHERE ACTOR_ID = :actorId")
+//    Actor read(@Bind("actorId") Long actorId);
+//
+//    @SqlQuery
+////    @SqlQuery("SELECT * from DEMO.ACTOR")
+//    List<Actor> readAll();
+//
+//  }
 
 }
--- src/main/java/com/example/demo/da/dao/ActorDao.java
+++ src/main/java/com/example/demo/da/dao/ActorDao.java
@@ -5,17 +5,24 @@ import com.example.demo.da.mappers.ActorMapper;
 import java.util.List;
 import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.jdbi.v3.stringtemplate4.UseStringTemplateSqlLocator;
 
-//@UseStringTemplateSqlLocator
 @RegisterRowMapper(ActorMapper.class)
-public interface ActorDaoMoved {
+@UseStringTemplateSqlLocator
+public interface ActorDao {
+
+  @SqlUpdate
+  @GetGeneratedKeys
+  long create(@BindBean Actor actor);
 
   @SqlQuery
   Actor read(@Bind("actorId") Long actorId);
 
-  @SqlQuery("SELECT * from DEMO.ACTOR")
+  @SqlQuery
   List<Actor> readAll();
 
 }
