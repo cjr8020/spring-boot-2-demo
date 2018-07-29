c6683422eddaccbd715eefcec303352cad79bcfe
cjr <cjr8020@gmail.com>
2018-07-29 16:17:21 -0700

    @EnableScheduling and @Scheduled

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,28 @@
 package com.example.demo;
 
+import java.util.Calendar;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoApplication {
+
+  private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
+  }
+
+  @Scheduled(
+      initialDelay = 1000,
+      fixedRate = 10000
+  )
+  public void scheduledCurrentTime() {
+    logger.info("Current time: {}", Calendar.getInstance().getTime());
   }
 }
